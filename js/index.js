4620618c0e7b6b5934e262fa50ec73ac902cb6e1
OHshajim <ajshajimmax@gmail.com>
2024-02-19 06:34:19 +0600

    add more features

--- js/index.js
+++ js/index.js
@@ -6,21 +6,26 @@
  * 4. show tickets
  */
 const seats = document.querySelectorAll(".seat-btn");
+const Tickets = document.querySelectorAll("tickets");
 const TicketCount = document.getElementById("count-ticket");
 const totalSeat = document.getElementById("total-seat");
 const Container = document.getElementById('container');
 const ticketContainer = document.getElementById('ticket-container');
-
+const couponInput = document.getElementById('coupon-input')
+const couponBTN = document.getElementById('coupon-btn')
+const couponPart = document.getElementById('coupon-part')
+const totalPrice = document.getElementById('total-price')
+const grandTotal = document.getElementById('grand-total')
 // values
 let countTicket = 0;
 let countSeat = 40;
+let price = 550;
 for (const seat of seats) {
     seat.addEventListener('click', function () {
-        console.log(seat.innerText);
         seat.classList.remove('bg-[#F7F8F8]');
         seat.classList.add('bg-[#1DD100]');
         seat.classList.add('text-white');
-
+        seat.disabled = true;
         // increment and decrement 
         countTicket++;
         countSeat--;
@@ -33,13 +38,32 @@ for (const seat of seats) {
                 seat.disabled = true;
             };
             alert('you cannot buy more than 4 tickets')
-            console.log(seat);
-        };
 
-        // show ticket-container
-        Container.classList.remove('hidden')
-        // create tickets
-        const p = document.createElement('p')
-        // p.innerText = 
+        };
+        // calculate money
+        totalPrice.innerText = price * countTicket;
+        grandTotal.innerText = totalPrice.innerText;
+        // show tickets
+        Container.classList.remove('hidden');
+        showTickets(seat)
+
+        // coupon part 
+
+        couponBTN.addEventListener('click', function () {
+            if (couponInput.value === "NEW15" && countTicket === 4) {
+                grandTotal.innerText =550*4-550*4*0.15;
+                couponPart.classList.add('hidden')
+            }
+            else if (couponInput.value === "Couple 20" && countTicket === 4) {
+                console.log("Couple 20");
+            }
+            else {
+                alert('invalid Coupon')
+                couponInput.value = "";
+                console.log(countTicket);
+            }
+
+        })
     })
 };
+
---
+++ js/unit.js
@@ -0,0 +1,18 @@
+// a function to show tickets 
+function showTickets(seat) {
+    const text = seat.innerText
+    const p = document.createElement('p');
+    p.innerHTML = text;
+    ticketContainer.appendChild(p);
+
+    const p1 = document.createElement('p');
+    p1.innerHTML ="Economy";
+    p1.classList.add('text-center')
+    ticketContainer.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.innerHTML = 550;
+    p2.classList.add('text-end')
+    ticketContainer.appendChild(p2);
+};
+{/* <span>Economy</span> <span>550</span> */ }
