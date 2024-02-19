2446bb76116d86efa5b9c0a1d9d17240ec3b1242
OHshajim <ajshajimmax@gmail.com>
2024-02-19 11:10:20 +0600

    fixed tickets

--- js/unit.js
+++ js/unit.js
@@ -7,11 +7,9 @@ function showTickets(seat) {
 
     const p1 = document.createElement('p');
     p1.innerHTML ="Economy";
-    p1.classList.add('text-center')
     ticketContainer.appendChild(p1);
 
     const p2 = document.createElement('p');
     p2.innerHTML = 550;
-    p2.classList.add('text-end');
     ticketContainer.appendChild(p2);
 };
